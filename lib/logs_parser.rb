34345f991918c7d18cd19e0b3d169af03306ca52
Grzegorz Unijewski <grzegorz@unijewski.pl>
2019-06-21 14:09:10 +0200

    Run Rubocop auto-correct

--- lib/logs_parser.rb
+++ lib/logs_parser.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "logs_parser/version"
 
 module LogsParser
--- spec/logs_parser_spec.rb
+++ spec/logs_parser_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe LogsParser do
   it "has a version number" do
     expect(LogsParser::VERSION).not_to be nil
