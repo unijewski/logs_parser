0f4587e0d36266b47503c689b2a7b97f230f8822
Grzegorz Unijewski <grzegorz@unijewski.pl>
2019-06-23 12:27:22 +0200

    Improve specs of LogsParser

--- spec/logs_parser_spec.rb
+++ spec/logs_parser_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe LogsParser do
   it "has a version number" do
-    expect(LogsParser::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(LogsParser::VERSION).not_to be(nil)
   end
 end
